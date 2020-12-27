17814c2d3647ca561a28000170caf0b3f56f180d
Suresh Kumar <ksuresh72945@gmail.com>
2020-12-27 22:14:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/messagecrypto/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.messagecrypto;
+
+
+//THIS IS AN CRYPTOGRAPHY POWERED ANDROID APPLICATION
+//IN THIS APPLICATION I WILL BE USING AED,DES,RSA,MD5 TO ENCRYPT AND DECRYPT MESSAGE
+//FOR STORING MESSAGE I WILL BE USING FIREBASE
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
