b0f9e29a0b81f6b60c689f7bb37040e66f1f89d8
Suresh Kumar <ksuresh72945@gmail.com>
2020-12-28 18:50:32 +0530

    Register Activity

--- app/src/main/java/com/example/messagecrypto/StartActivity.java
+++ app/src/main/java/com/example/messagecrypto/StartActivity.java
@@ -4,9 +4,11 @@ package com.example.messagecrypto;
 //THIS IS AN CRYPTOGRAPHY POWERED ANDROID APPLICATION
 //IN THIS APPLICATION I WILL BE USING AED,DES,RSA,MD5 TO ENCRYPT AND DECRYPT MESSAGE
 //FOR STORING MESSAGE I WILL BE USING FIREBASE
+//FOR THIS PROJECT THIS FILE WILL BE THE STARTING FILE
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 
 public class StartActivity extends AppCompatActivity {
 
