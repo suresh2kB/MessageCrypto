6871c514b6357c962f1f980f9cd05adb6fa86afb
Suresh Kumar <ksuresh72945@gmail.com>
2020-12-30 11:13:35 +0530

    Login Activity done

--- app/src/main/java/com/example/messagecrypto/StartActivity.java
+++ app/src/main/java/com/example/messagecrypto/StartActivity.java
@@ -7,14 +7,39 @@ package com.example.messagecrypto;
 //FOR THIS PROJECT THIS FILE WILL BE THE STARTING FILE
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button btn_register,btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
